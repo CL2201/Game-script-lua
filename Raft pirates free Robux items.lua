6114cd6604257015ab33b5a6b6b123f98ebf9d64
CL2201 <77050420+CL2201@users.noreply.github.com>
2021-08-15 15:52:46 +0100

    Add files via upload

---
+++ Raft pirates free Robux items.lua
@@ -0,0 +1,504 @@
+local Raftpiratesfreerobuxitems = Instance.new("ScreenGui")
+local Frame = Instance.new("ImageLabel")
+local Frame_2 = Instance.new("ImageLabel")
+local TextLabel = Instance.new("TextLabel")
+local TextButton = Instance.new("TextButton")
+local TextButton_Roundify_7px = Instance.new("ImageLabel")
+local TextButton_2 = Instance.new("TextButton")
+local TextButton_Roundify_7px_2 = Instance.new("ImageLabel")
+local TextButton_3 = Instance.new("TextButton")
+local TextButton_Roundify_7px_3 = Instance.new("ImageLabel")
+local TextButton_4 = Instance.new("TextButton")
+local TextButton_Roundify_7px_4 = Instance.new("ImageLabel")
+local TextButton_5 = Instance.new("TextButton")
+local TextButton_Roundify_7px_5 = Instance.new("ImageLabel")
+local TextButton_6 = Instance.new("TextButton")
+local TextButton_Roundify_7px_6 = Instance.new("ImageLabel")
+local TextButton_7 = Instance.new("TextButton")
+local TextButton_Roundify_7px_7 = Instance.new("ImageLabel")
+
+--Properties:
+
+Raftpiratesfreerobuxitems.Name = "Raft pirates free robux items"
+Raftpiratesfreerobuxitems.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+Raftpiratesfreerobuxitems.ResetOnSpawn = false
+
+Frame.Name = "Frame"
+Frame.Parent = Raftpiratesfreerobuxitems
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 1.000
+Frame.Position = UDim2.new(0.0617551506, 0, 0.170144454, 0)
+Frame.Size = UDim2.new(0, 282, 0, 411)
+Frame.Image = "rbxassetid://3570695787"
+Frame.ImageColor3 = Color3.fromRGB(24, 24, 24)
+Frame.ScaleType = Enum.ScaleType.Slice
+Frame.SliceCenter = Rect.new(100, 100, 100, 100)
+Frame.SliceScale = 0.050
+
+Frame_2.Name = "Frame"
+Frame_2.Parent = Frame
+Frame_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame_2.BackgroundTransparency = 1.000
+Frame_2.Size = UDim2.new(0, 282, 0, 49)
+Frame_2.Image = "rbxassetid://3570695787"
+Frame_2.ImageColor3 = Color3.fromRGB(45, 45, 45)
+Frame_2.ScaleType = Enum.ScaleType.Slice
+Frame_2.SliceCenter = Rect.new(100, 100, 100, 100)
+Frame_2.SliceScale = 0.050
+
+TextLabel.Parent = Frame_2
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Size = UDim2.new(0, 282, 0, 49)
+TextLabel.Font = Enum.Font.GothamBold
+TextLabel.Text = "Raft pirates free Robux items"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 59.000
+TextLabel.TextWrapped = true
+
+TextButton.Parent = Frame
+TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.BackgroundTransparency = 1.000
+TextButton.BorderSizePixel = 0
+TextButton.Position = UDim2.new(0.0354609936, 0, 0.180048659, 0)
+TextButton.Size = UDim2.new(0, 261, 0, 36)
+TextButton.ZIndex = 2
+TextButton.Font = Enum.Font.GothamBlack
+TextButton.Text = "Spawn all"
+TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextWrapped = true
+
+TextButton_Roundify_7px.Name = "TextButton_Roundify_7px"
+TextButton_Roundify_7px.Parent = TextButton
+TextButton_Roundify_7px.Active = true
+TextButton_Roundify_7px.AnchorPoint = Vector2.new(0.5, 0.5)
+TextButton_Roundify_7px.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_Roundify_7px.BackgroundTransparency = 1.000
+TextButton_Roundify_7px.Position = UDim2.new(0.5, 0, 0.5, 0)
+TextButton_Roundify_7px.Selectable = true
+TextButton_Roundify_7px.Size = UDim2.new(1, 0, 1, 0)
+TextButton_Roundify_7px.Image = "rbxassetid://3570695787"
+TextButton_Roundify_7px.ImageColor3 = Color3.fromRGB(85, 85, 85)
+TextButton_Roundify_7px.ScaleType = Enum.ScaleType.Slice
+TextButton_Roundify_7px.SliceCenter = Rect.new(100, 100, 100, 100)
+TextButton_Roundify_7px.SliceScale = 0.070
+
+TextButton_2.Parent = Frame
+TextButton_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_2.BackgroundTransparency = 1.000
+TextButton_2.BorderSizePixel = 0
+TextButton_2.Position = UDim2.new(0.0354609936, 0, 0.180000007, 44)
+TextButton_2.Size = UDim2.new(0, 261, 0, 36)
+TextButton_2.ZIndex = 2
+TextButton_2.Font = Enum.Font.GothamBlack
+TextButton_2.Text = "Spawn inf all"
+TextButton_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_2.TextScaled = true
+TextButton_2.TextSize = 14.000
+TextButton_2.TextWrapped = true
+
+TextButton_Roundify_7px_2.Name = "TextButton_Roundify_7px"
+TextButton_Roundify_7px_2.Parent = TextButton_2
+TextButton_Roundify_7px_2.Active = true
+TextButton_Roundify_7px_2.AnchorPoint = Vector2.new(0.5, 0.5)
+TextButton_Roundify_7px_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_Roundify_7px_2.BackgroundTransparency = 1.000
+TextButton_Roundify_7px_2.Position = UDim2.new(0.5, 0, 0.5, 0)
+TextButton_Roundify_7px_2.Selectable = true
+TextButton_Roundify_7px_2.Size = UDim2.new(1, 0, 1, 0)
+TextButton_Roundify_7px_2.Image = "rbxassetid://3570695787"
+TextButton_Roundify_7px_2.ImageColor3 = Color3.fromRGB(85, 85, 85)
+TextButton_Roundify_7px_2.ScaleType = Enum.ScaleType.Slice
+TextButton_Roundify_7px_2.SliceCenter = Rect.new(100, 100, 100, 100)
+TextButton_Roundify_7px_2.SliceScale = 0.070
+
+TextButton_3.Parent = Frame
+TextButton_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_3.BackgroundTransparency = 1.000
+TextButton_3.BorderSizePixel = 0
+TextButton_3.Position = UDim2.new(0.0354609936, 0, 0.180000007, 88)
+TextButton_3.Size = UDim2.new(0, 261, 0, 36)
+TextButton_3.ZIndex = 2
+TextButton_3.Font = Enum.Font.GothamBlack
+TextButton_3.Text = "Spawn -inf all"
+TextButton_3.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_3.TextScaled = true
+TextButton_3.TextSize = 14.000
+TextButton_3.TextWrapped = true
+
+TextButton_Roundify_7px_3.Name = "TextButton_Roundify_7px"
+TextButton_Roundify_7px_3.Parent = TextButton_3
+TextButton_Roundify_7px_3.Active = true
+TextButton_Roundify_7px_3.AnchorPoint = Vector2.new(0.5, 0.5)
+TextButton_Roundify_7px_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_Roundify_7px_3.BackgroundTransparency = 1.000
+TextButton_Roundify_7px_3.Position = UDim2.new(0.5, 0, 0.5, 0)
+TextButton_Roundify_7px_3.Selectable = true
+TextButton_Roundify_7px_3.Size = UDim2.new(1, 0, 1, 0)
+TextButton_Roundify_7px_3.Image = "rbxassetid://3570695787"
+TextButton_Roundify_7px_3.ImageColor3 = Color3.fromRGB(85, 85, 85)
+TextButton_Roundify_7px_3.ScaleType = Enum.ScaleType.Slice
+TextButton_Roundify_7px_3.SliceCenter = Rect.new(100, 100, 100, 100)
+TextButton_Roundify_7px_3.SliceScale = 0.070
+
+TextButton_4.Parent = Frame
+TextButton_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_4.BackgroundTransparency = 1.000
+TextButton_4.BorderSizePixel = 0
+TextButton_4.Position = UDim2.new(0.0354609936, 0, 0.180000007, 132)
+TextButton_4.Size = UDim2.new(0, 261, 0, 36)
+TextButton_4.ZIndex = 2
+TextButton_4.Font = Enum.Font.GothamBlack
+TextButton_4.Text = "Spawn Melee"
+TextButton_4.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_4.TextScaled = true
+TextButton_4.TextSize = 14.000
+TextButton_4.TextWrapped = true
+
+TextButton_Roundify_7px_4.Name = "TextButton_Roundify_7px"
+TextButton_Roundify_7px_4.Parent = TextButton_4
+TextButton_Roundify_7px_4.Active = true
+TextButton_Roundify_7px_4.AnchorPoint = Vector2.new(0.5, 0.5)
+TextButton_Roundify_7px_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_Roundify_7px_4.BackgroundTransparency = 1.000
+TextButton_Roundify_7px_4.Position = UDim2.new(0.5, 0, 0.5, 0)
+TextButton_Roundify_7px_4.Selectable = true
+TextButton_Roundify_7px_4.Size = UDim2.new(1, 0, 1, 0)
+TextButton_Roundify_7px_4.Image = "rbxassetid://3570695787"
+TextButton_Roundify_7px_4.ImageColor3 = Color3.fromRGB(85, 85, 85)
+TextButton_Roundify_7px_4.ScaleType = Enum.ScaleType.Slice
+TextButton_Roundify_7px_4.SliceCenter = Rect.new(100, 100, 100, 100)
+TextButton_Roundify_7px_4.SliceScale = 0.070
+
+TextButton_5.Parent = Frame
+TextButton_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_5.BackgroundTransparency = 1.000
+TextButton_5.BorderSizePixel = 0
+TextButton_5.Position = UDim2.new(0.0354609936, 0, 0.180000007, 176)
+TextButton_5.Size = UDim2.new(0, 261, 0, 36)
+TextButton_5.ZIndex = 2
+TextButton_5.Font = Enum.Font.GothamBlack
+TextButton_5.Text = "Spawn Tools"
+TextButton_5.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_5.TextScaled = true
+TextButton_5.TextSize = 14.000
+TextButton_5.TextWrapped = true
+
+TextButton_Roundify_7px_5.Name = "TextButton_Roundify_7px"
+TextButton_Roundify_7px_5.Parent = TextButton_5
+TextButton_Roundify_7px_5.Active = true
+TextButton_Roundify_7px_5.AnchorPoint = Vector2.new(0.5, 0.5)
+TextButton_Roundify_7px_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_Roundify_7px_5.BackgroundTransparency = 1.000
+TextButton_Roundify_7px_5.Position = UDim2.new(0.5, 0, 0.5, 0)
+TextButton_Roundify_7px_5.Selectable = true
+TextButton_Roundify_7px_5.Size = UDim2.new(1, 0, 1, 0)
+TextButton_Roundify_7px_5.Image = "rbxassetid://3570695787"
+TextButton_Roundify_7px_5.ImageColor3 = Color3.fromRGB(85, 85, 85)
+TextButton_Roundify_7px_5.ScaleType = Enum.ScaleType.Slice
+TextButton_Roundify_7px_5.SliceCenter = Rect.new(100, 100, 100, 100)
+TextButton_Roundify_7px_5.SliceScale = 0.070
+
+TextButton_6.Parent = Frame
+TextButton_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_6.BackgroundTransparency = 1.000
+TextButton_6.BorderSizePixel = 0
+TextButton_6.Position = UDim2.new(0.0354609936, 0, 0.180000007, 220)
+TextButton_6.Size = UDim2.new(0, 261, 0, 36)
+TextButton_6.ZIndex = 2
+TextButton_6.Font = Enum.Font.GothamBlack
+TextButton_6.Text = "Spawn Ranged"
+TextButton_6.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_6.TextScaled = true
+TextButton_6.TextSize = 14.000
+TextButton_6.TextWrapped = true
+
+TextButton_Roundify_7px_6.Name = "TextButton_Roundify_7px"
+TextButton_Roundify_7px_6.Parent = TextButton_6
+TextButton_Roundify_7px_6.Active = true
+TextButton_Roundify_7px_6.AnchorPoint = Vector2.new(0.5, 0.5)
+TextButton_Roundify_7px_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_Roundify_7px_6.BackgroundTransparency = 1.000
+TextButton_Roundify_7px_6.Position = UDim2.new(0.5, 0, 0.5, 0)
+TextButton_Roundify_7px_6.Selectable = true
+TextButton_Roundify_7px_6.Size = UDim2.new(1, 0, 1, 0)
+TextButton_Roundify_7px_6.Image = "rbxassetid://3570695787"
+TextButton_Roundify_7px_6.ImageColor3 = Color3.fromRGB(85, 85, 85)
+TextButton_Roundify_7px_6.ScaleType = Enum.ScaleType.Slice
+TextButton_Roundify_7px_6.SliceCenter = Rect.new(100, 100, 100, 100)
+TextButton_Roundify_7px_6.SliceScale = 0.070
+
+TextButton_7.Parent = Frame
+TextButton_7.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_7.BackgroundTransparency = 1.000
+TextButton_7.BorderSizePixel = 0
+TextButton_7.Position = UDim2.new(0.0354609936, 0, 0.180000007, 264)
+TextButton_7.Size = UDim2.new(0, 261, 0, 36)
+TextButton_7.ZIndex = 2
+TextButton_7.Font = Enum.Font.GothamBlack
+TextButton_7.Text = "Spawn Building"
+TextButton_7.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_7.TextScaled = true
+TextButton_7.TextSize = 14.000
+TextButton_7.TextWrapped = true
+
+TextButton_Roundify_7px_7.Name = "TextButton_Roundify_7px"
+TextButton_Roundify_7px_7.Parent = TextButton_7
+TextButton_Roundify_7px_7.Active = true
+TextButton_Roundify_7px_7.AnchorPoint = Vector2.new(0.5, 0.5)
+TextButton_Roundify_7px_7.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_Roundify_7px_7.BackgroundTransparency = 1.000
+TextButton_Roundify_7px_7.Position = UDim2.new(0.5, 0, 0.5, 0)
+TextButton_Roundify_7px_7.Selectable = true
+TextButton_Roundify_7px_7.Size = UDim2.new(1, 0, 1, 0)
+TextButton_Roundify_7px_7.Image = "rbxassetid://3570695787"
+TextButton_Roundify_7px_7.ImageColor3 = Color3.fromRGB(85, 85, 85)
+TextButton_Roundify_7px_7.ScaleType = Enum.ScaleType.Slice
+TextButton_Roundify_7px_7.SliceCenter = Rect.new(100, 100, 100, 100)
+TextButton_Roundify_7px_7.SliceScale = 0.070
+
+-- Scripts:
+
+local function ELUHSC_fake_script() -- TextButton.LocalScript 
+	local script = Instance.new('LocalScript', TextButton)
+
+	local Button = script.Parent
+	
+	Button.MouseButton1Click:Connect(function()
+		local amt = 1
+	
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Gem Pick"),
+			[2] = 1
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+		wait()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Godly Dagger"),
+			[2] = 1
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+		wait()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Shark Bone Saber"),
+			[2] = 1
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+		wait()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Hellfire Pistol"),
+			[2] = 1
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+		wait()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Rapture Musket"),
+			[2] = 1
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+		wait()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Explosive Cannon"),
+			[2] = 5
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+	end)
+end
+coroutine.wrap(ELUHSC_fake_script)()
+local function YLNGDZI_fake_script() -- TextButton_2.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_2)
+
+	local Button = script.Parent
+	
+	Button.MouseButton1Click:Connect(function()
+		local amt = math.huge
+	
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Gem Pick"),
+			[2] = amt
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+		wait()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Godly Dagger"),
+			[2] = amt
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+		wait()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Shark Bone Saber"),
+			[2] = amt
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+		wait()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Hellfire Pistol"),
+			[2] = amt
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+		wait()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Rapture Musket"),
+			[2] = amt
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+		wait()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Explosive Cannon"),
+			[2] = amt
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+	end)
+end
+coroutine.wrap(YLNGDZI_fake_script)()
+local function HICTOXS_fake_script() -- TextButton_3.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_3)
+
+	local Button = script.Parent
+	
+	Button.MouseButton1Click:Connect(function()
+		local amt = -math.huge
+	
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Gem Pick"),
+			[2] = amt
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+		wait()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Godly Dagger"),
+			[2] = amt
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+		wait()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Shark Bone Saber"),
+			[2] = amt
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+		wait()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Hellfire Pistol"),
+			[2] = amt
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+		wait()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Rapture Musket"),
+			[2] = amt
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+		wait()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Explosive Cannon"),
+			[2] = amt
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+	end)
+end
+coroutine.wrap(HICTOXS_fake_script)()
+local function QVOVO_fake_script() -- TextButton_4.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_4)
+
+	local Button = script.Parent
+	
+	Button.MouseButton1Click:Connect(function()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Godly Dagger"),
+			[2] = 1
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+		wait()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Shark Bone Saber"),
+			[2] = 1
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+	end)
+end
+coroutine.wrap(QVOVO_fake_script)()
+local function PDOQMTQ_fake_script() -- TextButton_5.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_5)
+
+	local Button = script.Parent
+	
+	Button.MouseButton1Click:Connect(function()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Gem Pick"),
+			[2] = 1
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+	end)
+end
+coroutine.wrap(PDOQMTQ_fake_script)()
+local function VQLV_fake_script() -- TextButton_6.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_6)
+
+	local Button = script.Parent
+	
+	Button.MouseButton1Click:Connect(function()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Hellfire Pistol"),
+			[2] = 1
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+		wait()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Rapture Musket"),
+			[2] = 1
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+	end)
+end
+coroutine.wrap(VQLV_fake_script)()
+local function IYEBVJ_fake_script() -- TextButton_7.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_7)
+
+	local Button = script.Parent
+	
+	Button.MouseButton1Click:Connect(function()
+		local args = {
+			[1] = game:GetService("ReplicatedStorage").Items:FindFirstChild("Explosive Cannon"),
+			[2] = 5
+		}
+	
+		game:GetService("ReplicatedStorage").Remotes.Interact.Drop:FireServer(unpack(args))
+	
+	end)
+end
+coroutine.wrap(IYEBVJ_fake_script)()
+local function FYZC_fake_script() -- Frame.LocalScript 
+	local script = Instance.new('LocalScript', Frame)
+
+	script.Parent.Active = true
+	script.Parent.Selectable = true
+	script.Parent.Draggable = true
+	
+end
+coroutine.wrap(FYZC_fake_script)()
